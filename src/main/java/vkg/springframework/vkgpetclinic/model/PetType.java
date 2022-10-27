bedf3dfd75cc7587013f7b1cd1f4ab6d230ffe5e
The Blue Moon <vishalkumargourav1@gmail.com>
2022-10-27 18:25:15 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/vkg/springframework/vkgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package vkg.springframework.vkgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/vkg/springframework/vkgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package vkg.springframework.vkgpetclinic.model;
+
+public class Vet extends Person {
+}
